b98dbc1474e6b5d20192c1ef85e7a1ab41215ea3
barack <barackannan00@gmail.com>
2026-04-28 10:16:57 +0300

    Implement Akan name generation functionality; add input fields and result display in index.html; enhance styles in styles.css

---
+++ code.js
@@ -0,0 +1,43 @@
+
+function generateName() {
+  let day = parseInt(document.getElementById("day").value);
+  let month = parseInt(document.getElementById("month").value);
+  let year = parseInt(document.getElementById("year").value);
+  let gender = document.getElementById("gender").value;
+
+  if (!day || !month || !year || gender === "") {
+    document.getElementById("result").innerText =
+      "Please fill all fields";
+    return;
+  }
+
+  let CC = Math.floor(year / 100);
+  let YY = year % 100;
+
+  let weekDay = Math.floor(
+    ((CC / 4) - 2 * CC - 1 + (5 * YY / 4) + (26 * (month + 1) / 10) + day
+    ) % 7
+  );
+
+  if (weekDay < 0) {
+    weekDay += 7;
+  }
+
+  let maleNames = [
+    "Kwasi", "Kwadwo", "Kwabena",
+    "Kwaku", "Yaw", "Kofi", "Kwame"
+  ];
+
+  let femaleNames = [
+    "Akosua", "Adwoa", "Abenaa",
+    "Akua", "Yaa", "Afua", "Ama"
+  ];
+
+  let akanName =
+    gender === "male"
+      ? maleNames[weekDay]
+      : femaleNames[weekDay];
+
+  document.getElementById("result").innerText =
+    "Your Akan Name is " + akanName;
+}
